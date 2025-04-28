76f260df2061c5c726ea557ba1bc0fda22fc69c4
Yossawat Imjai <yosimjai@z2t17c1.42bangkok.com>
2025-04-28 14:29:46 +0700

    add ex05

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,11 +1,7 @@
-echo $#
-if [ "$#" -eq 0 ];
-then
+if [ $# -eq 0 ]; then
   echo "No arguments supplied"
 else
-  i=1
-  while [$i -lt $#]
-  do
-    mkdir "ex$i"
+  for arg in "$@"; do
+    mkdir "ex$arg"
   done
 fi
