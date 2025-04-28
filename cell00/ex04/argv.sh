3b835d436a42b19c2ed2415f77d16e136739a03b
Yossawat Imjai <yosimjai@z2t17c1.42bangkok.com>
2025-04-28 19:12:03 +0700

    end shell start web

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,6 +1,15 @@
+cnt=1
 if [ "$#" -eq 0 ];
 then
     echo "No arguments supplied"
 else
-    echo $1 $2 $3
+	for i in $@
+	do
+		if [ $cnt -gt 3 ];
+		then
+			exit
+		fi
+		cnt=$((cnt+1))
+    		echo "$i"
+    	done
 fi
