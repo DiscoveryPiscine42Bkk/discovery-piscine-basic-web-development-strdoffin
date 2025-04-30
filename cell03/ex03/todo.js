d17e4ef712b2b3648c93ad7a2d099451682d13f2
Yossawat Imjai <yosimjai@z2t17c1.42bangkok.com>
2025-04-30 10:52:57 +0700

    addtodo

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -2,15 +2,17 @@ function popUpInput(){
     console.log('test')
     let todo = prompt('Enter your todo')
     let list = getCookie()
+    console.log(list)
     if(todo != null || todo != " "){
         if(list[0] == ""){
             setCookie(0,todo,2)
         }else{
             let data = list.at(-1)
             data = data.split('=')
-            data = data[0]
+            data = parseInt(data[0])+1
             setCookie(data,todo,2)
         }
+        location.reload()
     }
 }
 function setCookie(name,value,exday){
@@ -36,14 +38,16 @@ function getCookie(){
 function loadList(){
     let data = getCookie()
     for(let i=0;i<getCookieLen();i++){
-        const node = document.createElement("div");
-        const textnode = document.createTextNode(`${data[i]}`);
-        node.appendChild(textnode);
-        document.getElementById('ft_list').appendChild(node)
-        let text = data[i].split('=')
-        node.addEventListener('click',()=>{
-            removeCookie(text[0])
-        })
+        if(data[i] != ''){
+            const node = document.createElement("div");
+            const textnode = document.createTextNode(`${data[i].split('=')[1]}`);
+            node.appendChild(textnode);
+            document.getElementById('ft_list').appendChild(node)
+            let text = data[i].split('=')
+            node.addEventListener('click',()    =>{
+                removeCookie(text[0])
+            })
+        }
     }
 }
 
@@ -52,5 +56,6 @@ loadList()
 function removeCookie(x){
     if(confirm('Are you sure to Delete')){
         document.cookie = `${x}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+        location.reload()
     }
 }
