f039d6f6997a3dbc5cdef91a4e698e4c7ec71969
Yossawat Imjai <yosimjai@z2t17c4.42bangkok.com>
2025-05-02 04:16:26 +0700

    addskill

--- rush/yosimjai/index.js
+++ rush/yosimjai/index.js
@@ -1,26 +1,73 @@
 const sections = document.querySelectorAll('section');
 const navlis = document.querySelectorAll('nav ul li')
-window.addEventListener('scroll' , ()=>{
+window.addEventListener('scroll', () => {
     let cursection = ''
-    sections.forEach( (section) => {
+    sections.forEach((section) => {
         const sectionTop = section.offsetTop
         const sectionHeight = section.clientHeight
-        if(pageYOffset >= sectionTop-250){
-            cursection =  section.getAttribute('id')
+        if (pageYOffset >= sectionTop - 250) {
+            cursection = section.getAttribute('id')
         }
     })
-    navlis.forEach( (li) =>{
+    navlis.forEach((li) => {
         li.classList.remove('bg-orange-700')
-        if(li.getAttribute('id')== cursection+'Li'){
+        if (li.getAttribute('id') == cursection + 'Li') {
             li.classList.add('bg-orange-700')
         }
     })
 })
 
 
+var cnt = 0;
+var frontend = document.getElementById('frontend')
+var backend = document.getElementById('backend')
+var tool = document.getElementById('tool')
+
+
+function prevCard() {
+    frontend.classList.add('hidden')
+    backend.classList.add('hidden')
+    tool.classList.add('hidden')
+    cnt--
+    if (cnt < 0) {
+        cnt = 2
+    }
+    switch (cnt) {
+        case 0:
+            frontend.classList.remove('hidden')
+            return
+        case 1:
+            backend.classList.remove('hidden')
+            return
+        case 2:
+            tool.classList.remove('hidden')
+            return
+    }
+}
+function nextCard() {
+    frontend.classList.add('hidden')
+    backend.classList.add('hidden')
+    tool.classList.add('hidden')
+
+    cnt++
+    if (cnt > 2) {
+        cnt = 0
+    }
+    switch (cnt) {
+        case 0:
+            frontend.classList.remove('hidden')
+            return
+        case 1:
+            backend.classList.remove('hidden')
+            return
+        case 2:
+            tool.classList.remove('hidden')
+            return
+    }
+}
 
 
 
-function sent(){
+function sent() {
     alert('Sent Email Success!')
 }
