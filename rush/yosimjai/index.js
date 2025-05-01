bf8eddad71f51729de4fe2e2b17d0329bdba6277
Yossawat Imjai <yosimjai@z2t17c1.42bangkok.com>
2025-05-01 15:11:50 +0700

    latest

---
+++ rush/yosimjai/index.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll('section');
+const navlis = document.querySelectorAll('nav ul li')
+window.addEventListener('scroll' , ()=>{
+    let cursection = ''
+    sections.forEach( (section) => {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.clientHeight
+        if(pageYOffset >= sectionTop-250){
+            cursection =  section.getAttribute('id')
+        }
+    })
+    navlis.forEach( (li) =>{
+        li.classList.remove('bg-orange-700')
+        if(li.getAttribute('id')== cursection+'Li'){
+            li.classList.add('bg-orange-700')
+        }
+    })
+})
